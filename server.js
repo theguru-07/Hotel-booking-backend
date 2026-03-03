6fc68e0533e0c63d54b4cdd951f9df6f7b7bb77d
Malhar Prasad Ray <malharprasadray@gmail.com>
2026-03-03 16:02:49 +0530

    changed file name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import "dotenv/config"
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebHooks.js";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 
 connectDB()
